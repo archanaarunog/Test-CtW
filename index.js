a01b7c6acd7bee9336c7ca6294e697273eccae12
archanaarunog <archanaarunog@gmail.com>
2022-03-27 11:05:45 +0530

    public

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 var path = require('path')
-app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get("/", function(req,res)
@@ -11,7 +11,5 @@ app.get("/", function(req,res)
     //res.sendFile(__dirname + "/" + "main.html");
     //res.send("This is the second edit")
 })
-
-
 
 app.listen(process.env.PORT || 5000)
