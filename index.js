4f7297409004c0bc9bfd8ed2137ffa8238e65a4d
archanaarunog <archanaarunog@gmail.com>
2022-03-27 11:12:24 +0530

    /

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 var path = require('path')
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/css/')));
 
 
 app.get("/", function(req,res)
