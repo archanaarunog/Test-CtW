616e804ebc2312460472cbcc7477516b748a76d7
archanaarunog <archanaarunog@gmail.com>
2022-03-27 08:34:55 +0530

    added pdf display

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express")
 const app = express()
 
+var path = require('path');     
+var file = path.join(__dirname, 'file.pdf');    
+res.download(file, function (err) {
+       if (err) {
+           console.log("Error");
+           console.log(err);
+       } else {
+           console.log("Success");
+       }    
+});
+
 app.get("/", function(req,res)
 {
     res.sendFile(__dirname + "/" + "index.html");
